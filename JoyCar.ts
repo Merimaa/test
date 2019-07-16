f2669d76a50db7a614d041ec29ec756f4084ae7a
Merimaa <52958784+Merimaa@users.noreply.github.com>
2019-07-16 16:31:16 +0200

    Updates.

--- JoyCar.ts
+++ JoyCar.ts
@@ -1,1 +1,30 @@
- 
+namespace UltraSonicSensor {
+    //% blockId=device_show_number
+    //% block="show|number %v"
+    //% group="Ultra Sonic Sensor"
+    export function showNumber(v: number, interval: number = 150): void
+    {
+           //% group="Ultra Sonic Sensor"
+     }
+       //% group="Ultra Sonic Sensor"
+}
+
+namespace DCMotors {
+    //% blockId=device_show_number
+    //% block="show|number %v"
+    //% group="DCMotors"
+    export function showNumber(v: number, interval: number = 150): void {
+        //% group="DCMotors"
+    }
+    //% group="DCMotors"
+}
+
+namespace LEDS {
+    //% blockId=device_show_number
+    //% block="show|number %v"
+    //% group="LEDS"
+    export function showNumber(v: number, interval: number = 150): void {
+        //% group="LEDS"
+    }
+    //% group="LEDS"
+}
--- namespace.ts
+++ namespace.ts
@@ -1,7 +1,28 @@
 /**
- * JoyCar
+ * Ultra Sonic Sensor
  */
-//% color=#0fbc11 icon="\u272a" block="JoyCar"
-//% category="JoyCar"
-namespace JoyCar {
+//% color=#00ff2f weight=1000 icon="\u272a" block="Ultra Sonic Sensor"
+//% category="Ultra Sonic Sensor"
+namespace UltraSonicSensor {
+
 }
+
+/**
+ * DC Motors
+ */
+//% color=#0033ff weight=1000 icon="\u272a" block="DC Motors"
+//% category="DC Motors"
+namespace DCMotors {
+
+}
+
+/**
+ * LEDS
+ */
+//% color=#ff0000 weight=1000 icon="\u272a" block="LEDS"
+//% category="LEDS"
+namespace LEDS {
+
+}
+
+
