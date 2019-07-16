7fd0798edcf471df17a9c8e0041050460c93fbc7
Merimaa <52958784+Merimaa@users.noreply.github.com>
2019-07-16 16:35:36 +0200

    Updates.

--- JoyCar.ts
+++ JoyCar.ts
@@ -1,4 +1,18 @@
-namespace JoyCar {
+namespace Ultasonicsensor {
+    //% blockId=device_show_number
+    //% block="show|number %v"
+    //% group="Ultra Sonic Sensor"
+    export function showNumber(v: number, interval: number = 150): void
+    { }
+}
+namespace LEDS {
+    //% blockId=device_show_number
+    //% block="show|number %v"
+    //% group="Ultra Sonic Sensor"
+    export function showNumber(v: number, interval: number = 150): void
+    { }
+}
+ namespace Motors {
     //% blockId=device_show_number
     //% block="show|number %v"
     //% group="Ultra Sonic Sensor"
--- namespace.ts
+++ namespace.ts
@@ -1,9 +1,19 @@
 /**
- * JoyCar
+ * Ultasonicsensor
  */
-//% color=#0fbc11 icon="\u272a" block="JoyCar"
-//% category="JoyCar"
-//% groups=['LED', 'Ultra Sonic Sensor', 'DC Motors']
-namespace JoyCar {
+//% color=#00ff2f weight=1000 icon="\u272a" block="Ultasonicsensor"
+//% category="Ultasonicsensor"
+namespace Ultasonicsensor {
 
 }
+//% color=#0011ff weight=1000 icon="\u272a" block="Motors"
+//% category="Motors"
+namespace Motors {
+
+}
+//% color=#ff0015 weight=1000 icon="\u272a" block="LEDS"
+//% category="LEDS"
+namespace LEDS {
+
+}
+
